7075600a46a0bfe6abeb2dc12d8a690148dad56e
sagarbangade <sagarbangade55@gmail.com>
2025-03-23 13:19:29 +0530

    cros

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,10 @@
 // server.js
-const express = require('express');
-const cors = require('cors');
-const connectDB = require('./config/db');
-const employeeRoutes = require('./routes/employeeRoutes');
-const authRoutes = require('./routes/authRoutes');
-const path = require('path'); // Built-in Node.js module
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const employeeRoutes = require("./routes/employeeRoutes");
+const authRoutes = require("./routes/authRoutes");
+const path = require("path"); // Built-in Node.js module
 
 const app = express();
 
@@ -12,17 +12,16 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors()); // Enable CORS for all routes
+app.use(cors({ origin: ["https://employee-management-mern-phi.vercel.app"] })); // Enable CORS for all routes
 app.use(express.json()); // Parse JSON request bodies
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded request bodies
 
 // Define Routes
-app.use('/api/employees', employeeRoutes);
-app.use('/api/auth', authRoutes);
+app.use("/api/employees", employeeRoutes);
+app.use("/api/auth", authRoutes);
 
 // Serve static files from 'uploads' folder (optional, for development - not recommended for production)
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const PORT = process.env.PORT || 5000;
 
